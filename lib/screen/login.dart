ea11eff9332e78978534beeb6b92c5dee00df96f
Mison1212 <misonwenda20@gmail.com>
2025-10-31 13:29:03 +0700

    Buat ElevatedButton untuk Button Login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -9,79 +9,101 @@ class login extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          child:Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              const Icon(
-                Icons.lock_outline,
-                size: 100,
-                color: Colors.green
-              ),
-              const SizedBox(height: 10),
-              const Text('Selamat Datang',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-              ),
-              const SizedBox(height: 5),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const Icon(Icons.lock_outline, size: 100, color: Colors.green),
+                const SizedBox(height: 10),
+                const Text(
+                  'Selamat Datang',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 5),
 
-              const Text('Silahkan masuk untuk melanjutkan',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.green,
-              ),
-              ),
-              const SizedBox(height: 20),
+                const Text(
+                  'Silahkan masuk untuk melanjutkan',
+                  style: TextStyle(fontSize: 16, color: Colors.green),
+                ),
+                const SizedBox(height: 20),
 
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Masukkan Email Anda',
-                  prefixIcon: const Icon(Icons.email_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(
-                      color:Colors.blue,width: 2
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Masukkan Email Anda',
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
                     ),
                   ),
                 ),
-              ),
                 const SizedBox(height: 50),
 
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Masukkan password anda',
-                  prefixIcon: const Icon(Icons.lock_outline),
-                  suffixIcon: const Icon(Icons.visibility_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Masukkan password anda',
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    suffixIcon: const Icon(Icons.visibility_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                  ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(
-                      color:Colors.blue,width: 2
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Lupa Password',
+                      style: TextStyle(color:Colors.blue,fontSize: 14),
+                      
+                    ),
+                    ),
+                    
                 ),
-              ),
-               enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(color: Colors.grey),
-                ),
-                ),
-              ),
-            ],
-          ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    elevation: 2,
+                  ),
+                   child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                   ),
+                   ),
+              ],
+            ),
           ),
         ),
       ),
