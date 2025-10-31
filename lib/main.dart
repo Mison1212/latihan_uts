fb3733029b6e3fdae25383135d406d424fcbb431
Mison1212 <misonwenda20@gmail.com>
2025-10-31 10:03:54 +0700

    tambahkan tombol continue

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_uts/screen/splash_screen_1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      // home: SplashScreen1(),
+      home: SplashScreen1(),
       debugShowCheckedModeBanner: false,
     );
   }
